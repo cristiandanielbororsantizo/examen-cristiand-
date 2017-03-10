987a70fbd06c7cca92640a5da076cd8261067fd8
unknown <estudiante@LAB0236.SERVERINT.LOCAL>
2017-03-10 12:06:32 -0600

    cristian

---
+++ funciones.js
@@ -0,0 +1,10 @@
+var bacoB.module=angular.module("bacoB",{});
+bacoB.controller("mostrar",function ($scope){
+      $scope.mensaje="boror"
+      $scope.operaciones= ($scope.first)*($scope.second)
+       $scope.first;=15
+       $scope.secod;=20
+       
+
+
+}
